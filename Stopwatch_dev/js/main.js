884610741f26321e34d6638507dc100b1232388d
zovalger <74253343+zovalger@users.noreply.github.com>
2022-07-26 13:56:59 -0400

    Delete Stopwatch_dev directory

--- Stopwatch_dev/js/main.js
+++
@@ -1,9 +0,0 @@
-import StopwatchUI from "./modules/Stopwatch.UI.js";
-
-const c = new StopwatchUI();
-
-
-document.querySelector('.nav-toggle-theme')
-            .addEventListener("click", () => {
-                document.body.classList.toggle("dark-theme") 
-            })
